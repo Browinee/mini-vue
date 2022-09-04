3a3ea446f8403ae511b69144f65babcb09e39e12
justin <>
2022-09-04 19:01:18 +0800

    feat: add reactive

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,39 @@
+let activeEffect;
+
+class ReactiveEffect {
+  private _fn: any;
+  constructor(fn) {
+    this._fn = fn;
+  }
+  run() {
+    activeEffect = this;
+    this._fn();
+  }
+}
+const globalTargetMap = new Map();
+export function track(target, key) {
+  // {[target]: {[key]: dep[]}
+  let depsMap = globalTargetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    globalTargetMap.set(target, depsMap);
+  }
+
+  let deps = depsMap.get(key);
+  if (!deps) {
+    deps = new Set();
+    depsMap.set(key, deps);
+  }
+  deps.add(activeEffect);
+}
+export function trigger(target, key) {
+  let depsMap = globalTargetMap.get(target);
+  let deps = depsMap.get(key);
+  for (const effect of deps) {
+    effect.run();
+  }
+}
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
+}
