fa4c5b61aa961a7aa8994e09893da4a113b6a436
User <user@AdmindeMacBook-Air.local>
2022-09-06 13:11:35 +0800

    test: add readonly test

---
+++ src/reactivity/tests/readonly.spec.ts
@@ -0,0 +1,18 @@
+import { readOnly } from "../reactive";
+
+describe("readOnly", () => {
+  it("happy path", () => {
+    const original = { foo: 1, bar: { baz: 2 } };
+    const wrapped = readOnly(original);
+    expect(wrapped).not.toBe(original);
+    expect(wrapped.foo).toBe(1);
+  });
+  it("warn then call set", () => {
+    console.warn = jest.fn();
+    const user = readOnly({
+      age: 10,
+    });
+    user.age = 11;
+    expect(console.warn).toBeCalled();
+  });
+});
