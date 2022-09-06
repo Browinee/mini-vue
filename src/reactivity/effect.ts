a27c34fe374ec964f2cf3dc240a57ca0c2910fbc
User <user@AdmindeMacBook-Air.local>
2022-09-06 11:23:34 +0800

    feat: add onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,8 +1,13 @@
+import { extend } from "./shared";
+
 let activeEffect;
 
 class ReactiveEffect {
   private _fn: any;
   public scheduler?: () => void;
+  private _active: boolean = true;
+  public onStop?: () => void;
+  deps = [];
   constructor(fn, scheduler) {
     this._fn = fn;
     this.scheduler = scheduler;
@@ -11,10 +16,23 @@ class ReactiveEffect {
     activeEffect = this;
     return this._fn();
   }
+  stop() {
+    // NOTE: check if stop is called before
+    if (this._active) {
+      cleanupEffect(this);
+      this.onStop && this.onStop();
+      this._active = false;
+    }
+  }
+}
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
 }
 const globalTargetMap = new Map();
 export function track(target, key) {
-  // {[target]: {[key]: dep[]}
+  // NOTE: {[target]: {[key]: dep[]}
   let depsMap = globalTargetMap.get(target);
   if (!depsMap) {
     depsMap = new Map();
@@ -26,7 +44,9 @@ export function track(target, key) {
     deps = new Set();
     depsMap.set(key, deps);
   }
+  if (!activeEffect) return;
   deps.add(activeEffect);
+  activeEffect.deps.push(deps);
 }
 export function trigger(target, key) {
   let depsMap = globalTargetMap.get(target);
@@ -39,11 +59,21 @@ export function trigger(target, key) {
     }
   }
 }
-export function effect(fn, options?: { scheduler?: () => void }) {
-  const { scheduler = () => {} } = options || {};
-  const _effect = new ReactiveEffect(fn, scheduler);
+export function effect(
+  fn,
+  options?: { scheduler?: () => void; onStop: () => void }
+) {
+  const { scheduler } = options || {};
+  const _effect: any = new ReactiveEffect(fn, scheduler);
+  extend(_effect, options);
   _effect.run();
-  return () => _effect.run();
-  // or use following way to bind this
-  // return _effect.run.bind(_effect);
+
+  // NOTE: _effect.run need correct this
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
